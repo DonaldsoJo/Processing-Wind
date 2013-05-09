b70f0442eb1d2cf40068d2e50f0c1152b8b62e9e
DonaldsoJo <john.m.donaldson@hp.com>
2013-05-09 13:01:20 +0200

    the sketch sources

---
+++ WindTests/src/main/MyProcessingSketch.java
@@ -0,0 +1,42 @@
+package main;
+import processing.core.*;
+import wind.WindCell;
+import wind.WindMatrix;
+
+public class MyProcessingSketch extends PApplet
+{
+	
+	private static final long serialVersionUID = 1L;
+	
+	WindMatrix _matrix;
+
+	public void setup() {
+		// TODO Auto-generated method stub
+		
+	    size(200,200);
+	    background(0);
+	    
+	    _matrix = new WindMatrix(3,3);
+	    _matrix.setCell(0,0,10,0);
+	    _matrix.setCell(0,1,10,0);
+	    _matrix.setCell(0,2,10,0);
+	}
+	
+	public void draw() {
+		// draw matrix
+		WindCell[][] cells = _matrix.cells();
+		
+		for(int col=0; col<cells.length; col++ )
+			for(int row=0; row<cells[col].length; row++)
+			{
+				drawCell( cells[col][row]);
+			}			
+		
+		// update matrix
+		_matrix.updateCells();
+	}
+
+	private void drawCell(WindCell cell) {
+		println("draw cell");
+	}
+}
---
+++ WindTests/src/main/Start.java
@@ -0,0 +1,11 @@
+package main;
+
+import processing.core.PApplet;
+
+public class Start {
+
+	public static void main(String[] args) {
+	    PApplet.main(new String[] { "--present", "main.MyProcessingSketch" });
+	}
+
+}
